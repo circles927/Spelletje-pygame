b40abdf9fc5d8b97e2596448ff1448a5b7403231
circles927 <niels.magendans@gmail.com>
2025-11-12 16:23:19 +0100

    added shooting lasers one at a time

--- mainGame.py
+++ mainGame.py
@@ -1,7 +1,5 @@
 import pygame
 import json
-import sys
-import os
 # import time
 
 class SpriteSheet:
@@ -28,6 +26,9 @@ def main():
     SPRITESHEET_PATH = "images/sprite 2.png"
     JSON_PATH = "images/sprite 2.json"
 
+    LASERSPRITESHEET = "images/laser.png"
+    LASERSPRITEJSON = "images/laser.json"
+
     sheet = SpriteSheet(SPRITESHEET_PATH, JSON_PATH)
 
     framesRight = [sheet.get_frame("sprite 2 0.aseprite"), sheet.get_frame("sprite 2 1.aseprite")]
@@ -36,15 +37,23 @@ def main():
     framesJumpRight = [sheet.get_frame("sprite 2 2.aseprite"), sheet.get_frame("sprite 2 3.aseprite"), sheet.get_frame("sprite 2 4.aseprite")]
     framesJumpLeft = [pygame.transform.flip(framesJumpRight[0], True, False), pygame.transform.flip(framesJumpRight[1], True, False), pygame.transform.flip(framesJumpRight[2], True, False)]
 
+    laser = SpriteSheet(LASERSPRITESHEET, LASERSPRITEJSON)
+
+    laserFramesRight = [laser.get_frame("laser 0.aseprite"), laser.get_frame("laser 1.aseprite"), laser.get_frame("laser 2.aseprite")]
+    laserFramesLeft = [pygame.transform.flip(laserFramesRight[0], True, False), pygame.transform.flip(laserFramesRight[1], True, False), pygame.transform.flip(laserFramesRight[2], True, False)]
+
     frame_index = 0
-    frame_timer = 0
-    frame_delay = 100
 
     direction = "right"
     jump = "none"
+    fired = "none"
+    laserDirection = "none"
 
     posX = 150
     posY = 100
+
+    laserX = 0
+    laserY = 0
 
     running = True
     while running:
@@ -53,24 +62,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            if event.type == pygame.MOUSEBUTTONUP:
+                button = event.button
 
         keys = pygame.key.get_pressed()
-
-        # Update animation
-        # frame_timer += dt
-        # if frame_timer >= frame_delay:
-        #     frame_timer = 0
-        #     frame_index = (frame_index + 1) % len(frames)
+        mouseButtons = pygame.mouse.get_pressed()
 
         # Draw already starts here:
         screen.fill((30, 30, 30))
 
         # Update animation on pressing button, and blitting left or right
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_d]:
             posX += 4
             frame_index = (frame_index + 1) % len(framesRight)
             direction = "right"
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_a]:
             posX -= 4
             frame_index = (frame_index + 1) % len(framesLeft)
             direction = "left"
@@ -145,10 +152,52 @@ def main():
                 posY += 10
                 screen.blit(framesJumpLeft[0], (posX, posY))
                 jump = "none"
-
         else:
             print("something went wrong tracking direction")
 
+        if mouseButtons[0] == True:
+            if fired == "none" and direction == "right":
+                laserX = posX + 50
+                laserY = posY + 28
+                screen.blit(laserFramesRight[0], (laserX, laserY))
+                fired = "started"
+                laserDirection = "right"
+            elif fired == "none" and direction == "left":
+                laserX = posX + 40
+                laserY = posY + 28
+                screen.blit(laserFramesLeft[0], (laserX, laserY))
+                fired = "started"
+                laserDirection = "left"
+
+        if fired != "none" and laserDirection == "right":
+            if fired == "started":
+                laserX += 3
+                screen.blit(laserFramesRight[1], (laserX, laserY))
+                fired = "halfway"
+            elif fired == "halfway":
+                laserX += 9
+                screen.blit(laserFramesRight[2], (laserX, laserY))
+                fired = "full"
+            elif fired == "full":
+                laserX += 9
+                screen.blit(laserFramesRight[2], (laserX, laserY))
+                if laserX > screen.get_width():
+                    fired = "none"
+        elif fired != "none" and laserDirection == "left":
+            if fired == "started":
+                laserX -= 3
+                screen.blit(laserFramesRight[1], (laserX, laserY))
+                fired = "halfway"
+            elif fired == "halfway":
+                laserX -= 9
+                screen.blit(laserFramesRight[2], (laserX, laserY))
+                fired = "full"
+            elif fired == "full":
+                laserX -= 9
+                screen.blit(laserFramesRight[2], (laserX, laserY))
+                if laserX < 0:
+                    fired = "none"  
+          
         pygame.display.flip()
     
     pygame.quit()
